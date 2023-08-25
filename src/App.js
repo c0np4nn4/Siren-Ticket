1ac1ba48889036f18cce5c05f837803eb45678e6
c0np4nn4 <c0np4nn4@gmail.com>
2023-08-26 07:47:53 +0900

    wip

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import QRCodeGenerator from './QRCodeGenerator';
 import QRCodeScanner from './QRCodeScanner'; // QRCodeScanner 컴포넌트 파일을 import
 
 const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<QRCodeGenerator />} /> {/* QRCodeGenerator 페이지 */}
-        <Route path="/qrcodescanner" element={<QRCodeScanner />} /> {/* QRCodeScanner 페이지 */}
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<QRCodeGenerator />} /> {/* QRCodeGenerator 페이지 */}
+                <Route path="/qrcodescanner" element={<QRCodeScanner />} /> {/* QRCodeScanner 페이지 */}
+            </Routes>
+        </Router>
+    );
 };
 
 export default App;
